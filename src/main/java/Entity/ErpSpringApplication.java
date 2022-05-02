778816479fa93cc1452c98eac10e4178913f8a23
chiaratortorella <chiara.tortorella001@gmail.com>
2022-05-02 12:13:59 +0200

    chiara

---
+++ src/main/java/Entity/ErpSpringApplication.java
@@ -0,0 +1,13 @@
+package Entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpSpringApplication.class, args);
+    }
+
+}
---
+++ src/main/java/Repository/repositoryPersona.java
@@ -0,0 +1,9 @@
+package Repository;
+
+import Entity.Persona;
+
+import java.util.List;
+
+public class repositoryPersona {
+    }
+
